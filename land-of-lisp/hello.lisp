262dd17018b91ec57d5cb725cd8a4b8c9df23124
hiyoki <maha.tree.21@gmail.com>
2018-01-20 17:16:54 +0900

    add array sample

--- land-of-lisp/hello.lisp
+++ land-of-lisp/hello.lisp
@@ -13,3 +13,19 @@
               (double-cheeze (list 'patty 'patty 'cheeze))))
           order))
 
+;; array sample
+(defparameter x (make-array 3))
+
+;; generic setter
+(setf (aref x 1) 'foo)
+
+;; hash table sample
+(defparameter y (make-hash-table))
+(setf (gethash 'yup y) '25)
+
+;; 複数の値を返すことができる
+(round 2.4)
+
+(defun hukusu()
+  (values 3 7))
+
