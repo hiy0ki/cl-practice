d508d128edaa1eb58fa1800e2c87c62a7c12bba2
hiyoki <maha.tree.21@gmail.com>
2018-01-20 11:25:45 +0900

    add mapcan sample

--- land-of-lisp/hello.lisp
+++ land-of-lisp/hello.lisp
@@ -3,3 +3,13 @@
   (let ((name (read)))
     (print "Nice to meet you, ")
     (print name)))
+
+;; mapcan sample
+(defun ingredients (order)
+  (mapcan (lambda (burger)
+            (case burger
+              (single (list 'patty))
+              (double (list 'patty 'patty))
+              (double-cheeze (list 'patty 'patty 'cheeze))))
+          order))
+
