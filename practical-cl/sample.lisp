073834433c6d0c665b47c9e2810b0e91150bfc81
hiyoki <maha.tree.21@gmail.com>
2018-02-05 23:50:34 +0900

    add list sample

--- practical-cl/sample.lisp
+++ practical-cl/sample.lisp
@@ -31,3 +31,29 @@
 (min (3) 5) ; error
 (min 3 t) ; error
 
+;; vector
+;; 変更はできない
+(vector) ; #()
+(vector 1) ; #(1)
+(vector 1 2 3) ; #(1 2 3)
+
+(make-array 5) ; #(0 0 0 0 0)
+(make-array 5 :fill-pointer 0) ; #()
+
+;; higher order function
+(count-if #'evenp #(1 2 3 4 5))
+
+(remove-if-not #'oddp #((1 a) (2 b) (3 c) (4 d) (5 e)) :key #'car)
+
+(sort (vector "foo" "bar" "baz") #'string<)
+(sort '(54 23 64 22) #'<)
+(sort '(54 23 64 22) #'>)
+
+;; sequence
+(subseq "foobarbaz" 3) ; "barbaz"
+(subseq "foobarbaz" 3 6) ; "bar"
+
+
+;; list
+(append '(1 2 3) '(4 5 6))
+
