269bf957bec4c70a24dc03e9368cc92cf9227382
hiyoki <maha.tree.21@gmail.com>
2018-02-10 23:07:51 +0900

    add class sample

--- practical-cl/class-sample.lisp
+++ practical-cl/class-sample.lisp
@@ -17,4 +17,31 @@
   (withdraw (proxied-account proxy) amount))
 
 
+;; classを定義する
+;; slotとはfieldのこと
+;; 多重継承は可
+(defclass bank-account ()
+  (customer-name
+  balance))
+
+(defclass checking-account (bank-account)
+  )
+
+(defclass saving-account (bank-account)
+  )
+
+;; classの使い方
+;; objectの作り方
+(make-instance 'bank-account)
+
+;; use example
+;; set
+(defparameter *account* (make-instance 'bank-account))
+(setf (slot-value *account* 'customer-name) "John Doe")
+(setf (slot-value *account* 'balance) 1000)
+
+;; get
+(slot-value *account* 'customer-name) ; John Doe
+(slot-value *account* 'balance) ; 1000
+
 
