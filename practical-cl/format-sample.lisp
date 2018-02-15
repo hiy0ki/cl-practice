b8f88c931aadb1c46730e2957ae79b7964cf2d7d
hiyoki <maha.tree.21@gmail.com>
2018-02-15 13:23:20 +0900

    update format sample

--- practical-cl/format-sample.lisp
+++ practical-cl/format-sample.lisp
@@ -19,5 +19,39 @@
 
 (format t "~:@d" 1000000000)
 
+;; ~a支持子
+(format t "The value is: ~a" 10)
+
+(format t "The value is: ~a" "foo")
+
+(format t "The value is: ~a" '(1 2 3))
+
+;; ~r
+(format t "~r" 1234)
+
+(format t "file~p" 1)
+(format t "file~p" 10)
+(format t "file~p" 0)
+
+(format t "~r file~:p" 1)
+(format t "~r file~:p" 10)
+(format t "~r file~:p" 0)
+
+(format t "~r famil~:@p" 1)
+(format t "~r famil~:@p" 10)
+(format t "~r famil~:@p" 0)
+
+;; 大文字小文字
+(format t "~(~a~)" "FOO")
+(format t "~:@(~a~)" "foo")
+
+;; 反復
+(format t "~{~a, ~}" '(1 2 3)) ; 1, 2, 3,
+(format t "~{~a~^, ~}" '(1 2 3)) ; 1, 2, 3
+
+
+
+
+
 
 
